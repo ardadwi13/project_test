93e3990338a60b941ed3556cb00face3a0e125e3
ardadwi13 <januararda13@gmail.com>
2021-07-31 14:43:09 +0700

    Improve Input-1

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <html>
-    <title>Projek Test</title>
-    <link rel="stylesheet" href="style.css">
-    <body>
-    <?php
-error_reporting(0);
-?>
+<title>Projek Test</title>
+<link rel="stylesheet" href="style.css">
+<body>
+<?php error_reporting(0);?>
 <!-- input1 -->
-
-<?php
-$string1 = $_POST['text'];
-$newString1 = '';
-$length = strlen($string1);
-for ($i=0 ; $i<$length ; $i++) {
-    if (isset($_POST['submit'])&&($string1[$i] >= 'A' && $string1[$i] <= 'Z')) {
-        $newString1 .= strtolower($string1[$i]);
-    } else if (isset($_POST['submit'])&&($string1[$i] >= 'a' && $string1[$i] <= 'z')) {
-        $newString1 .= strtoupper($string1[$i]);
-    } else {
-        $newString1 .= $string1[$i];
-    }
+<?php 
+$myText = $_POST['text'];
+function LowerCaseUpperCase($myText){
+    $split = str_split($myText);
+    foreach($split as $s){
+       if(isset($_POST['submit'])&&($s >= 'A' && $s <= 'Z' )){
+            echo strtolower($s);
+       }elseif(isset($_POST['submit'])&&($s >= 'a' && $s <= 'z')){
+        echo strtoupper($s); 
+       }else{
+           echo $s;
+        }
+     }
 }
+ob_start();
+LowerCaseUpperCase($myText);
+$output = ob_get_clean();
 ?>
+
 <!-- input2 -->
 <?php 
 $string2 = $_POST['text'];
@@ -32,11 +34,10 @@ $trans = array("a"=> "","e"=>"","i"=>"","o"=>"","u"=>"","A"=> "",
 
 <div class="form-action">
 <form action="index.php" method="post">
-    <h2>Input </h2>
+<h2>Input </h2>
 <input type="text" name="text" class="input" value=""/>
-<!-- <input type="submit" name="submit" value="submit"> -->
 </br>
-<p class="output"><?php echo"<b>Output : </b>".$newString1; ?></p>
+<p class="output"><?php echo"<b>Output : </b>".$output; ?></p>
 </br>
 <p class="output"><?php echo"<b>Output : </b> ".strtr($string2,$trans);?></p>
 </br>
@@ -44,4 +45,5 @@ $trans = array("a"=> "","e"=>"","i"=>"","o"=>"","u"=>"","A"=> "",
 </form>
 </div>
 </body>
+</html>
 
